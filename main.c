4126b307f57a733f366f439efdf334e22a2a6116
GalTechDev <poussigalitv@gmail.com>
2024-09-26 08:31:46 +0200

    Update main.c

--- main.c
+++ main.c
@@ -19,16 +19,21 @@ void list_demo() {
   append(table, line);    // append a list
 
   // Clear a list
-  //clear(line); not implemented !!
+  printf("before list clear : %s\n", toString(line)); 
+  clear(line);
+  printf("after list clear : %s\n", toString(line)); 
 
-  /* Not implemented !!
-  // Insert elements to the end of a list
-  insert(line, 0, nb1);     // append an integer
-  insert(line, 0, nbfloat);  // append a float
-  insert(line, 0, nbtext);   // append a string
-  insert(table, 0, line);    // append a list
-  */
+  set(line, 0, nb1);     // append an integer
+  set(line, 1, nbfloat);  // append a float
+  //set(line, 2, nbtext);   // append a string
+  set(table, 0, line);    // append a list
 
+  printf("after list set : %s\n", toString(line)); 
+
+  insert(line, 1, nbtext);     // insert a string
+
+  printf("after list insert : %s\n", toString(line)); 
+  
   // Access elements in the list
   Object *nb2 = get(line, 0); // get a Element
 
@@ -36,17 +41,20 @@ void list_demo() {
   len(line);
 
   // Print information
-  printf("%d\n", len(line));  // print the number of elements in the list
-  printf("%s\n", toString(nb2));      // print an object (<value>)
-  printf("%s\n", toString(get(line, 0))); // print an element in the list [<value1>, <value2>, ...]
-  printf("%s\n", toString(table));       // print a list of lists
+  printf("len of list %d\n", len(line));  // print the number of elements in the list
+  printf("element nb2 : %s\n", toString(nb2));      // print an object (<value>)
+  printf("element of line[0] : %s\n", toString(get(line, 0))); // print an element in the list [<value1>, <value2>, ...]
+  printf("liste 'table' : %s\n", toString(table));       // print a list of lists
 
   // Free memory
   Object *nb1_copy = pop(line, 0);
   del(line, 0);
 
-  printf("%s\n", toString(nb1_copy));
-  printf("%s\n", toString(table));
+  printf("'nb1_copy' the result of pop 0: %s\n", toString(nb1_copy));
+  printf("liste 'table' : %s\n", toString(table));
+  printf("index 0 existe ? %d\n", existe(line, 0));
+  clear(line);
+  printf("index 0 existe ? (after clear) %d\n", existe(line, 0));
 }
 
 void dict_demo() {
@@ -63,14 +71,19 @@ void dict_demo() {
   Object *nbfloat = floating(5.9);
 
   // Clear a dict
-  //clear(dico1); not implemented !!
+  set(dico1, string("nb1"), nb1);     // append an integer
+  set(dico1, string("nbfloat"), nbfloat);  // append a float
+  
+  printf("before dict clear : %s\n", toString(dico1)); 
+  clear(dico1);
+  printf("after dict clear : %s\n", toString(dico1)); 
 
   // Insert elements to the end of a dict
-  insert(dico1, string("nb1"), nb1);     // append an integer
-  insert(dico1, string("nbfloat"), nbfloat);  // append a float
-  insert(dico1, string("nbtext"), nbtext);   // append a string
-  insert(dico1, string("line"), line);   // append a list
-  insert(dico2, string("dico1"), dico1);    // append a dict
+  set(dico1, string("nb1"), nb1);     // append an integer
+  set(dico1, string("nbfloat"), nbfloat);  // append a float
+  set(dico1, string("nbtext"), nbtext);   // append a string
+  set(dico1, string("line"), line);   // append a list
+  set(dico2, string("dico1"), dico1);    // append a dict
 
   // Access elements in the list
   Object *nb2 = get(line, 0); // get a Element
@@ -79,23 +92,29 @@ void dict_demo() {
   len(dico1);
 
   // Print information
-  printf("%d\n", len(dico1));  // print the number of elements in the list
-  printf("%s\n", toString(nb2));      // print an object <value>
-  printf("%s\n", toString(get(dico1, string("line")))); // print an element in the dict
-  printf("%s\n", toString(dico2));       // print a dict of dict
+  printf("len of dict : %d\n", len(dico1));  // print the number of elements in the list
+  printf("element nb2 : %s\n", toString(nb2));      // print an object <value>
+  printf("element of dico1['line'] : %s\n", toString(get(dico1, string("line")))); // print an element in the dict
+  printf("dict 'dico2' : %s\n", toString(dico2));       // print a dict of dict
 
   // Free memory
   Object *nb1_copy = pop(dico1, string("nbtext"));
   del(dico1, string("nbfloat"));
 
-  printf("%s\n", toString(nb1_copy));
-  printf("%s\n", toString(dico1));
+  printf("'nb1_copy' result of pop 'nbtext' : %s\n", toString(nb1_copy));
+  printf("dict 'dico1' : %s\n", toString(dico1));
+  printf("nb1 existe ? %d\n", existe(dico1, string("nb1")));
+  printf("nb2 existe ? %d\n", existe(dico1, string("nb2")));
+
+  printf("keys of 'dico1' %s\n", toString(keys(dico1)));
+  printf("values of 'dico1' %s\n", toString(values(dico1)));
+  printf("items of 'dico1' %s\n", toString(items(dico1)));
 }
 
 int main() {
 
   //list_demo();
-  //dict_demo();
+  dict_demo();
 
   return 0;
 }
